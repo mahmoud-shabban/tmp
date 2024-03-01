38614954ef77027ad09a0e0f02a7c1dc615e4864
Mahmoud Shabban <mahmoudshabban300@gmail.com>
2024-03-02 00:37:16 +0200

    dsf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "my_jenkins_tf_instance" {
   instance_type = "t2.micro"
-  ami           = "ami-005e54dee72cc1d00"
+  ami           = "ami-0d18e50ca22537278"
   subnet_id     = "subnet-0f527dee344292b31"
 }
